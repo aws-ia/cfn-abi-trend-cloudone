7771720b168464836612cf0bffc04191b68199f6
Felipe Costa <33869171+felipecosta09@users.noreply.github.com>
2023-05-18 13:15:58 -0500

    updates requested by Kishore (#12)

--- .project_automation/functional_tests/entrypoint.sh
+++ .project_automation/functional_tests/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -ex
 
-
 ## NOTE: paths may differ when running in a managed task. To ensure behavior is consistent between
 # managed and local tasks always use these variables for the project and project type path
 PROJECT_PATH=${BASE_PATH}/project
@@ -9,6 +8,16 @@ PROJECT_TYPE_PATH=${BASE_PATH}/projecttype
 cd ${PROJECT_PATH}
 
 regions=(us-east-1 us-east-2 us-west-2 us-west-1)
+
+for region in ${regions[@]}
+do
+    echo "Cleanup running in region: $region"
+    export AWS_DEFAULT_REGION=$region
+    python3 scripts/cleanup_config.py -C scripts/cleanup_config.json
+done
+
+taskcat test run -t security-hub 
+
 for region in ${regions[@]}
 do
     echo "Cleanup running in region: $region"
@@ -18,10 +27,18 @@ done
 
 echo $AWS_DEFAULT_REGION
 unset AWS_DEFAULT_REGION
-
 echo $AWS_DEFAULT_REGION
-# Run taskcat e2e test
-taskcat test run
+
+taskcat test run -t ws-ssm-deployment
+
+for region in ${regions[@]}
+do
+    echo "Cleanup running in region: $region"
+    export AWS_DEFAULT_REGION=$region
+    python3 scripts/cleanup_config.py -C scripts/cleanup_config.json
+done
+
+taskcat test run -t security-hub-no-abi
 
 ## Executing ash tool
 
@@ -34,4 +51,3 @@ taskcat test run
 
 #ash --source-dir .
 #cat aggregated_results.txt
-
